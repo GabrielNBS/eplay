7f77695b0a1d712a235e9a405ef9881ec37c7fa2
GabrielNBS <123820384+GabrielNBS@users.noreply.github.com>
2023-11-01 18:28:24 -0300

    feat: new Hero component

---
+++ src/components/Hero/styles.ts
@@ -0,0 +1,58 @@
+import styled from 'styled-components'
+import { cores } from '../../styles'
+import { TagContainer } from '../Tag/styles'
+
+export const Banner = styled.div`
+  display: block;
+  position: relative;
+  height: 480px;
+  width: 100%;
+  background-repeat: no-repeat;
+  background-size: 100%;
+  background-position: center;
+  padding-top: 16px;
+
+  &::after {
+    content: '';
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    left: 0;
+    top: 0;
+    background-color: ${cores.preto};
+    opacity: 0.56;
+  }
+
+  ${TagContainer} {
+    margin-right: 8px;
+  }
+
+  .container {
+    z-index: 1;
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+    justify-content: space-between;
+  }
+`
+export const Infos = styled.div`
+  padding: 16px;
+  background-color: ${cores.preto};
+  max-width: 290px;
+  font-weight: bold;
+
+  h2 {
+    font-size: 32px;
+  }
+
+  p {
+    font-size: 18px;
+    margin: 16px 0;
+
+    span {
+      display: block;
+      text-decoration: line-through;
+    }
+  }
+`
