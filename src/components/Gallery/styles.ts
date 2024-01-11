f30534194d0c46c7d2d28ca0d90292add36f73dd
GabrielNBS <123820384+GabrielNBS@users.noreply.github.com>
2024-01-11 14:07:37 -0300

    chore: change Loading interactions

--- src/components/Gallery/styles.ts
+++ src/components/Gallery/styles.ts
@@ -74,6 +74,12 @@ export const ModalContent = styled.div`
     display: flex;
     justify-content: space-between;
     margin-bottom: 24px;
+
+    img {
+      height: 16px;
+      width: 16px;
+      cursor: pointer;
+    }
   }
 
   h4 {
